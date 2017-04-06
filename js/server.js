160892ad413de35a3a6c652497056925ec0bec26
WORKSHOP_00 <thomas.jung@sap.com>
2017-04-06 12:09:57 -0400

    Updated for HANA 2.0 SPS 01 and UI5 1.44

--- js/server.js
+++ js/server.js
@@ -1,28 +1,36 @@
 /*eslint no-console: 0, no-unused-vars: 0*/
 "use strict";
 
-var xsjs  = require("sap-xsjs");
-var xsenv = require("sap-xsenv");
-var port  = process.env.PORT || 3000;
+var xsjs = require("@sap/xsjs");
+var xsenv = require("@sap/xsenv");
+var port = process.env.PORT || 3000;
 var server = require("http").createServer();
 var express = require("express");
-var app = express();  
+var app = express();
 
-var options = xsjs.extend({
-//	anonymous : true, // remove to authenticate calls
-	redirectUrl : "/index.xsjs"
-});
+var options = {
+	//	anonymous : true, // remove to authenticate calls
+	redirectUrl: "/index.xsjs"
+};
 
 // configure HANA
 try {
-	options = Object.assign(options, xsenv.getServices({ hana: {tag: "hana"} }));
+	options = Object.assign(options, xsenv.getServices({
+		hana: {
+			tag: "hana"
+		}
+	}));
 } catch (err) {
 	console.log("[WARN]", err.message);
 }
 
 // configure UAA
 try {
-	options = Object.assign(options, xsenv.getServices({ uaa: {tag: "xsuaa"} }));
+	options = Object.assign(options, xsenv.getServices({
+		uaa: {
+			tag: "xsuaa"
+		}
+	}));
 } catch (err) {
 	console.log("[WARN]", err.message);
 }
@@ -31,6 +39,6 @@ var xsjsApp = xsjs(options);
 app.use(xsjsApp);
 
 server.on("request", app);
-server.listen(port, function(){
-	console.log("HTTP Server: " + server.address().port );
+server.listen(port, function() {
+	console.log("HTTP Server: " + server.address().port);
 });
